1da27e6ceb0db70d943e46b4e26bd913cdc421f1
YkseBay <xavssbbbenoit@gmail.com>
2024-07-02 12:50:15 -0400

    dictionnaire: get et updateNode

--- calque-app-tsx/frontend/src/controllers/dictionnaire.tsx
+++ calque-app-tsx/frontend/src/controllers/dictionnaire.tsx
@@ -1,9 +1,12 @@
-import { Node } from "../../../backend/src/models/to_move/node";
+import { Edge } from "../models/edge";
+import { Ligne } from "../models/ligne";
+import { Node } from "../models/node";
+import { Style } from "../models/style";
 
 
 export class Dictionnaire {
     private static instance: Dictionnaire;
-    private _registre: Map<number, Node> = new Map;
+    private _registre: Map<number, Node | Edge> = new Map;
 
     private constructor() {}
 
@@ -24,11 +27,66 @@ export class Dictionnaire {
         return typeof nextId === undefined ? 0 : nextId! + 1;
     }
 
+    // get
+    public get(key: number): Edge | Node | undefined {
+        return this._registre.get(key);
+    }
+
     // create node
     public createNode(): number {
         const id: number = this.getNextId();
         const node: Node = new Node("node " + id.toString(), 100, 100);
         this._registre.set(id, node);
         return id;
+    }
+
+    // update node
+    public updateNode({key, name, posX, posY, style, addEntrant, removeEntrant, addSortant, removeSortant}:
+        {key: number,
+            name?: string,
+            posX?: number, 
+            posY?: number, 
+            style?: Style, 
+            addEntrant?: Edge[], 
+            removeEntrant?: Edge[], 
+            addSortant?: Edge[], 
+            removeSortant?: Edge[]}) {
+
+        let node: Node | Edge | undefined = this.get(key);
+        if (typeof node !== "undefined" && Object.keys(node).includes("_entrant")) {
+            node = (node as Node);
+            if (typeof name !== "undefined") {
+                node.name = name;
+            }
+            if (typeof posX !== "undefined") {
+                node.posX = posX;
+            }
+            if (typeof posY !== "undefined") {
+                node.posY = posY;
+            }
+            if (typeof style !== "undefined") {
+                node.style = style;
+            }
+            if (typeof addEntrant !== "undefined") {
+                for (const edge of addEntrant) {
+                    node.addEntrant(edge);
+                }
+            }
+            if (typeof removeEntrant !== "undefined") {
+                for (const edge of removeEntrant) {
+                    node.removeEntrant(edge);
+                }
+            }
+            if (typeof addSortant !== "undefined") {
+                for (const edge of addSortant) {
+                    node.addSortant(edge);
+                }
+            }
+            if (typeof removeSortant !== "undefined") {
+                for (const edge of removeSortant) {
+                    node.removeSortant(edge);
+                }
+            }
+        }
     }
 }
