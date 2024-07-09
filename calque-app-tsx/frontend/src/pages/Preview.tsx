5e5076e9c4dd691eb5f4d09afbd394a59384adf8
divcamillediv <camille.divisia@hotmail.com>
2024-07-09 03:09:28 -0400

    preview mode

--- calque-app-tsx/frontend/src/pages/Preview.tsx
+++ calque-app-tsx/frontend/src/pages/Preview.tsx
@@ -1,12 +1,38 @@
-import Map from "../components/Map"
-function Preview() {
+import Map2 from "../components/Map2"
+import Map3 from "../components/Map3"
+
+import { ReactNode, useState } from "react"
+
+/**
+ * Preview qui trace un chemin de Laurier à Parc
+ */
+
+interface mapProps {
+  clickMap: () => void;
+}
+
+const ReturnMap2: React.FC<mapProps> = (props) => {
   return (
-    <>
-    <div>Preview</div>
-    <Map/>
-    </>
+    <div>
+      <button onClick={props.clickMap}><Map2 /></button>
+    </div>
+  );
+};
+
+function Preview() {
+
+  const [mapClicked, setMapClicked] = useState<boolean>(true);
+
+  function clickMap() {
+    setMapClicked(!mapClicked)
+  }
+
+  return (
     
-  )
+    mapClicked ? <ReturnMap2 clickMap={clickMap} /> : <Map3 />
+     
+    
+  );
 }
 
 export default Preview
