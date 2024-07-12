e72f408c079256380eaefe27725f98d9d1e99d25
divcamillediv <camille.divisia@hotmail.com>
2024-07-12 17:01:28 -0400

    refactorisation mode switcher

--- calque-app-tsx/frontend/src/components/ModeSwitcher.tsx
+++ calque-app-tsx/frontend/src/components/ModeSwitcher.tsx
@@ -7,22 +7,29 @@ import { MdPreview } from "react-icons/md";
 import { ModeContext } from './Layout';
 
 function ModeSwitcher() {
-    const [mode, setMode] = useContext(ModeContext)
+    const [mode, setMode] = useContext(ModeContext);
+
+    const toggleMode = () => setMode(mode === 'editor' ? 'preview' : 'editor');
+
+    const editorIconStyle = mode === 'editor' ? "w-8 h-8" : "w-8 h-8 opacity-50";
+    const previewIconStyle = mode === 'preview' ? "w-8 h-8" : "w-8 h-8 opacity-50";
 
     return (
         <div className="flex justify-end">
             <div className="flex flex-wrap items-center gap-4">
-                    <FaRegEdit className="w-8 h-8"/>
-                    <button onClick={() => {setMode(mode === 'editor' ? 'preview' : 'editor')}}>
-                        {mode === "preview" && <LiaToggleOnSolid className='w-6 h-6'/>}
-                        {mode === "editor" && <LiaToggleOffSolid className='w-6 h-6'/>}
-                    </button>
-                    
-                    {/*<Link to="/Preview"><FaPlay className="w-8 h-8"/></Link>*/}
-                    <MdPreview className="w-8 h-8"/>
+                <FaRegEdit className={editorIconStyle} />
+                <button onClick={toggleMode}>
+                    {mode === 'preview' ? <LiaToggleOnSolid className='w-6 h-6'/> : <LiaToggleOffSolid className='w-6 h-6'/>}
+                </button>
+                {/*
+                <Link to="/Preview">
+                    <FaPlay className="w-8 h-8"/>
+                </Link>
+                */}
+                <MdPreview className={previewIconStyle} />
             </div>
         </div>
-    )
+    );
 }
 
-export default ModeSwitcher
+export default ModeSwitcher;
